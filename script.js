3c1f30a36a4db3d58d7838887fd04adf64581af8
jamlicker <k.mateusz78@gmail.com>
2022-09-29 19:43:14 +0200

    Big progres on start
    ^^^

--- script.js
+++ script.js
@@ -6,15 +6,39 @@ window.addEventListener('load', function(){
   canvas.height = 500;
 
   class InputHandler {
-    constructor() {
-
+    constructor(game) {
+      this.game = game;
+      window.addEventListener('keydown', e => {
+        if (( (e.key === 'ArrowUp') ||
+              (e.key === 'ArrowDown') )
+        &&  this.game.keys.indexOf(e.key) === -1 ){
+            this.game.keys.push(e.key);
+        }
+        console.log(this.game.keys);
+      });
+      window.addEventListener('keyup', e => {
+        if (this.game.keys.indexOf(e.key) > -1){
+          this.game.keys.splice(this.game.keys.indexOf(e.key),1);
+        }
+        console.log(this.game.keys);
+      });
     }
   }
 
   class Projectile {
-    constructor() {
-
+    constructor(game, x, y) {
+      this.game = game;
+      this.x = x;
+      this.y = y;
+      this.width = 10;
+      this.height = 3;
+      this.speed = 3;
+      this.markedForDeletion = false;
     }
+    update() {
+      this.x += this.speed;
+      if (this.x > this.game.width * 0.8) this.markedForDeletion = true;
+    
   }
 
   class Particle {
@@ -31,8 +55,12 @@ window.addEventListener('load', function(){
       this.x = 20;
       this.y = 100;
       this.speedY = 0;
+      this.maxSpeed = 2;
     }
     update(){
+      if (this.game.keys.includes('ArrowUp')) this.speedY = -1;
+      else if (this.game.keys.includes('ArrowDown')) this.speedY = 1;
+      else this.speedY = 0;
       this.y +=this.speedY;
     }
     draw(context){
@@ -63,19 +91,32 @@ window.addEventListener('load', function(){
 
     }
   }
-
   class Game {
     constructor(width, height) {
       this.width = width;
       this.height = height;
       this.player = new Player(this);
-
+      this.input = new InputHandler(this);
+      this.keys = [];
+  }
+    update(){
+      this.player.update();
+    }
+    draw(context){
+      this.player.draw(context);
     }
   }
 
 
-
-
+const game = new Game(canvas.width, canvas.height);
+//animation loop
+function animate(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  game.update();
+  game.draw(ctx);
+  requestAnimationFrame(animate);
+}
+animate();
 
 
 });
